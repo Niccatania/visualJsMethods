186e654b09bf3e99e04e5e98909b26d49b627eee
Niccatania <Niccatania6@gmail.com>
2022-10-23 19:51:46 -0500

    add background and text styling

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
-var a = ["Boom", "Bam", "Bizzy"];
-document.getElementById("myId").innerHTML = a;
 
-function myFunction() {
-    a.push("Bop");
-    document.getElementById("myId").innerHTML = a;
+var a = [];
+document.getElementById("Arr").innerHTML = a;
+
+function pushVis() {
+    a.push("&#128525");
+    document.getElementById("Arr").innerHTML = a;
 }
+
+function unshiftVis() {
+    a.unshift("&#128512");
+    document.getElementById("Arr").innerHTML = a;
+}
+
+function popVis() {
+    a.pop("&#128525");
+    document.getElementById("Arr").innerHTML = a;
+}
+
+function shiftVis() {
+    a.shift("&#128512");
+    document.getElementById("Arr").innerHTML = a;
+}
+const characters =' &#128516;  &#128151;';
+
